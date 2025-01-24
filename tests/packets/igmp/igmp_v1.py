ecbff74f64efc32057c4c11e5fbb05f0b18b8b5c
Rodrigo Nardi <rnardi@netdef.org>
2025-01-24 15:57:07 -0300

    topotest: Sending IGMPv1 packet

--- tests/packets/igmp/igmp_v1.py
+++ tests/packets/igmp/igmp_v1.py
@@ -24,17 +24,24 @@ Usage Example:
     # Create an IGMPv1 packet without Router Alert
     igmp_packet = IGMPv1(gaddr="224.0.0.1")
     igmp_packet.show()
-
-    # Enable Router Alert
-    igmp_packet.enable_router_alert()
-    igmp_packet.show()
-
-    # Send the packet on the network interface (e.g., eth0)
-    sendp(igmp_packet, iface="eth0")
 """
+import struct
 
-from scapy.all import Packet, ByteField, ShortField, IPField, ConditionalField
-from scapy.sendrecv import sendp
+from scapy.all import Packet, ByteField, IPField
+from scapy.fields import XShortField, ByteEnumField, BitField
+from scapy.layers.inet import IP, ICMP, IPOption, IPOption_Router_Alert
+from scapy.layers.l2 import Ether
+from scapy.packet import bind_layers
+from scapy.sendrecv import sendp, sr1
+
+
+def calculate_checksum(packet):
+    if len(packet) % 2 == 1:
+        packet += b'\0'
+    s = sum(struct.unpack("!%dH" % (len(packet) // 2), packet))
+    s = (s >> 16) + (s & 0xffff)
+    s += s >> 16
+    return ~s & 0xffff
 
 
 class IGMPv1(Packet):
@@ -42,35 +49,57 @@ class IGMPv1(Packet):
     Represents an IGMPv1 packet.
 
     Attributes:
-        type (int): The type of the IGMP message (default is 0x11).
-        max_resp_time (int): The maximum response time (default is 0).
+        version (4-bits): IGMP version (default is 0x1).
+        type (4-bits): The type of the IGMP message (default is 0x11).
+        unused (8-bits): The maximum response time (default is 0).
         checksum (int): The checksum of the packet (default is None).
         gaddr (str): The group address (default is "0.0.0.0").
         router_alert (int): The Router Alert field (default is 0x0, conditional).
 
     Methods:
-        enable_router_alert(value=0x94):
-            Enables the Router Alert field with the specified value.
+
     """
 
     name = "IGMPv1"
     fields_desc = [
-        ByteField("type", 0x11),
-        ByteField("max_resp_time", 0),
-        ShortField("checksum", None),
-        IPField("gaddr", "0.0.0.0"),
-        ConditionalField(ByteField("router_alert", 0x0), lambda pkt: hasattr(pkt, 'router_alert_enabled') and pkt.router_alert_enabled)
+        BitField("version", 1, 4),
+        BitField("type", 0x11, 4),
+        ByteField("unused", 0),
+        XShortField("chksum", None),
+        IPField("gaddr", "0.0.0.0")
     ]
 
-    def enable_router_alert(self, value=0x9404):
-        """
-        Enables the Router Alert field with the specified value.
+    def __init__(self, version=1, type=0x11, unused=0, chksum=None, gaddr="0.0.0.0", src_ip="192.168.100.1", *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.version = version
+        self.type = type
+        self.unused = unused
+        self.chksum = chksum
+        self.gaddr = gaddr
+        self.src_ip = src_ip
+        self.options = []
 
-        Args:
-            value (int): The value to set for the Router Alert field (default is 0x9404).
-        """
-        self.router_alert = value
-        self.router_alert_enabled = True
+    def enable_router_alert(self):
+        router_alert = IPOption_Router_Alert()
+        self.options.append(router_alert)
+
+    def post_build(self, p, pay):
+        if self.chksum is None:
+            chksum = calculate_checksum(p)
+            p = p[:2] + struct.pack("!H", chksum) + p[4:]
+        return p + pay
 
     def send(self, interval=0, count=1, iface="eth0"):
-        sendp(self, inter=int(interval), iface=iface, count=int(count))
+        bind_layers(IP, IGMPv1)
+
+        if self.options:
+            packet = Ether() / IP(dst=self.gaddr, tos=0xc0, id=0, ttl=1, src=self.src_ip, options=self.options, proto=2, frag=0) / self
+        else:
+            packet = Ether() / IP(dst=self.gaddr, tos=0xc0, id=0, ttl=1, src=self.src_ip, proto=2, frag=0) / self
+
+        sendp(packet, inter=int(interval), iface=iface, count=int(count))
+
+if __name__ == "__main__":
+    igmp_packet = IGMPv1(gaddr="224.1.3.2", src_ip="192.168.1.10", type=0x12)
+    igmp_packet.enable_router_alert()
+    igmp_packet.send(iface="r1-eth0", count=1)
--- tests/topotests/igmp_v1_without_router_alert/test_igmp_v1_without_router_alert.py
+++ tests/topotests/igmp_v1_without_router_alert/test_igmp_v1_without_router_alert.py
@@ -12,6 +12,7 @@
 import os
 import sys
 from itertools import count
+from time import sleep
 
 CWD = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(CWD, "../../packets/igmp"))
@@ -19,7 +20,7 @@ sys.path.append(os.path.join(CWD, "../../packets/igmp"))
 # pylint: disable=C0413
 from lib import topotest
 from lib.topogen import Topogen, TopoRouter, get_topogen
-from lib.topolog import logger
+from lib.topolog import logger, get_test_logdir
 from igmp_v1 import IGMPv1
 
 #####################################################
@@ -28,18 +29,14 @@ from igmp_v1 import IGMPv1
 ##
 #####################################################
 
+HOST_IP = "192.168.1.10"
+
 def build_topo(tgen):
-    tgen.add_router("r1")
+    router = tgen.add_router("r1")
 
-    # On main router
-    # First switch is for a dummy interface (for local network)
-    switch = tgen.add_switch("sw1")
-    tgen.add_host("h1", "192.168.100.100/24", "via 192.168.100.1")
-    tgen.add_host("h2", "192.168.100.100/24", "via 192.168.100.2")
+    tgen.add_host("h1", f"{HOST_IP}/24", "via 192.168.1.1")
 
-    switch.add_link(tgen.gears["r1"])
-    switch.add_link(tgen.gears["h1"])
-    switch.add_link(tgen.gears["h2"])
+    router.add_link(tgen.gears["h1"])
 
 #####################################################
 ##
@@ -72,12 +69,25 @@ def test_send_igmp_v1():
 
     tgen = get_topogen()
 
-    host_obj, interface_name = tgen.gears["sw1"].links['sw1-eth1']
+    tgen.gears['r1'].vtysh_cmd("debug igmp")
+    tgen.gears['r1'].vtysh_cmd("debug pim")
+
+    host_obj, interface_name = tgen.gears["r1"].links['r1-eth0']
+
+    logger.info(tgen.gears['r1'].vtysh_cmd("show interfaces"))
+    logger.info(get_test_logdir())
 
     logger.info("Sending IGMPv1 packet without Router Alert")
-    igmp_packet = IGMPv1(gaddr="224.0.0.1")
-    igmp_packet.send(iface=interface_name.split("-")[1], count=3)
+    igmp_packet = IGMPv1(gaddr="224.1.3.2", type=0x12, src_ip=HOST_IP)
+    igmp_packet.enable_router_alert()
 
+    sleep(40)
+
+    igmp_packet.send(iface=interface_name.split("-")[1], count=5)
+    logger.info("IGMPv1 packet sent successfully")
+
+    logger.info(tgen.gears['r1'].vtysh_cmd("show ip igmp groups"))
+    logger.info(tgen.gears['r1'].vtysh_cmd("show ip igmp join"))
 
 if __name__ == "__main__":
     args = ["-s"] + sys.argv[1:]
